0f9126382c08a1fc2b1bcda40e3c3a6c8594d601
oliveiraigordev <igor.oliveira42@gmail.com>
2024-07-24 18:02:22 -0300

    test: testes unitarios no repositorio pets

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,3 +1,4 @@
+import pytest
 from src.models.sqlite.settings.connection import db_connection_hander
 from .pets_repository import PetsRepository
 from .people_repository import PeopleRepository
@@ -5,12 +6,21 @@ from .people_repository import PeopleRepository
 
 db_connection_hander.connect_to_db()
 
+@pytest.mark.skip(reason='interacao com o banco de dados')
 def test_list_pets():
     repo = PetsRepository(db_connection_hander)
     response = repo.list_pets()
     print(response)
 
+@pytest.mark.skip(reason='interacao com o banco de dados')
 def test_list_people():
     repo = PeopleRepository(db_connection_hander)
     response = repo.list_people()
     print(response)
+
+@pytest.mark.skip(reason='interacao com o banco de dados')
+def test_delete_pet():
+    name = "belinha"
+
+    repo = PetsRepository(db_connection_hander)
+    repo.delete_pets(name)
