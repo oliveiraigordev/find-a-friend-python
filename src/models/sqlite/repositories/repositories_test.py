390ba493dcd4f8a78a66e44f884585b2413f04cb
oliveiraigordev <igor.oliveira42@gmail.com>
2024-07-25 15:30:52 -0300

    feat: inclusao de metodos no repositorio People

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,3 +1,6 @@
+'''
+Utilizado somente para didática, para uso profissional não é recomendado ter este arquivo
+'''
 import pytest
 from src.models.sqlite.settings.connection import db_connection_hander
 from .pets_repository import PetsRepository
@@ -24,3 +27,22 @@ def test_delete_pet():
 
     repo = PetsRepository(db_connection_hander)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason='interacao com o banco de dados')
+def test_insert_person():
+    first_name = "test name"
+    last_name = "test last"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_hander)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason='interacao com o banco de dados')
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_hander)
+    response = repo.get_person(person_id)
+    print(response)
+    print(response.pet_name)
