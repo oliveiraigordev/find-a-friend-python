79e4f4ed8f2633345c85f7fe0494da9a6316eae3
oliveiraigordev <igor.oliveira42@gmail.com>
2024-07-26 15:20:37 -0300

    feat: implementacao de controller para exclusao de pet

---
+++ src/controllers/pet_deleter_controller_test.py
@@ -0,0 +1,9 @@
+from .pet_deleter_controller import PetDeleterController
+
+
+def test_delete_pet(mocker):
+    mocker_repository = mocker.Mock()
+    controller = PetDeleterController(mocker_repository)
+    controller.delete("amiguinho")
+
+    mocker_repository.delete_pets.assert_called_once_with("amiguinho")
